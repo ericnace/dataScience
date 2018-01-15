3a40ba816da0068da83f2a43d717abd0f1f2339d
Eric Nace <ericnace@hotmail.com>
2018-01-14 21:59:35 -0800

    completed chapter 1

--- Chapter1.py
+++ Chapter1.py
@@ -1,48 +1,61 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jan  6 09:50:04 2018
-
-@author: ericn
-"""
+from __future__ import division
 
 # -*- coding: utf-8 -*-
 """
 Created on Mon Jan  1 17:10:15 2018
-
 @author: ericn
 """
-from __future__ import division
+
 import numpy as np
-from matplotlib.pyplot import *
-
-
+import matplotlib.pyplot as plt
+from collections import Counter, defaultdict # not loaded by default
+from Chapter1Functions import getNumFriends, number_of_friends, \
+ friends_of_friend_ids_bad, friends_of_friend_ids, \
+ most_common_interests_with, \
+ data_scientists_who_like
 
 users = [ 
          {"id": 0, "name": "Hero"},
-         {"id": 1, "name": "Hero"},
-         {"id": 2, "name": "Hero"},
-         {"id": 3, "name": "Hero"},
-         {"id": 4, "name": "Hero"},
-         {"id": 5, "name": "Hero"},
-         {"id": 6, "name": "Hero"},
-         {"id": 7, "name": "Hero"},
-         {"id": 8, "name": "Hero"},
-         {"id": 9, "name": "Hero"}
+         {"id": 1, "name": "Dunn"},
+         {"id": 2, "name": "Sue"},
+         {"id": 3, "name": "Chi"},
+         {"id": 4, "name": "Thor"},
+         {"id": 5, "name": "Clive"},
+         {"id": 6, "name": "Hicks"},
+         {"id": 7, "name": "Devin"},
+         {"id": 8, "name": "Kate"},
+         {"id": 9, "name": "Klein"}
         ]
 
 friendships =[(0,1),(0,2),(1,2),(1,3),(2,3),(3,4),
               (4,5),(5,6),(5,7),(6,8),(7,8),(8,9)]
 
+salaries_and_tenures = [(83000, 8.7), (88000, 8.1),
+                        (48000, 0.7), (76000, 6),
+                        (69000, 6.5), (76000, 7.5),
+                        (60000, 2.5), (83000, 10),
+                        (48000, 1.9), (63000, 4.2)]
+
+def tenure_bucket(tenure):
+    if tenure < 2: return "less than two"
+    elif tenure < 5: return "between two and five"
+    else: return "more than five"
+    
+def make_chart_salaries_by_tenure():
+    tenures = [tenure for salary, tenure in salaries_and_tenures]
+    salaries = [salary for salary, tenure in salaries_and_tenures]
+    plt.scatter(tenures, salaries)
+    plt.xlabel("Years Experience")
+    plt.ylabel("Salary")
+    plt.show()
+    
 for user in users:
     user["friends"] = []
     
-for i, j in friendships:
-    users[i]["friends"].append(users[j])
-    users[j]["friends"].append(users[i])
+for i, ji in friendships:
+    users[i]["friends"].append(users[ji])
+    users[ji]["friends"].append(users[i])
     
-def number_of_friends(user):
-    #"""How many friends does _user_ have?"""
-    return len(user["friends"])
 
 total_connections = sum(number_of_friends(user) 
     for user in users)
@@ -50,10 +63,112 @@ total_connections = sum(number_of_friends(user)
 num_users = len(users)
 avg_connections = total_connections / num_users
 
-num_friends_by_id = [(users["id"], number_of_friends(user))
+num_friends_by_id = [(user["id"], number_of_friends(user))
     for user in users]
 
-sorted(num_friends_by_id, 
-       key=lambda (user_id, num_friends): num_friends,
-       reverse=True)
 
+for person in num_friends_by_id:
+    print(getNumFriends(person))
+    
+
+num_friends_by_id1 = sorted(num_friends_by_id,
+             key= getNumFriends, # by number of friends
+             reverse=True) 
+
+print()
+
+for person in num_friends_by_id1:
+    print(getNumFriends(person))
+    
+print()
+    
+[print(friends_of_friend_ids_bad(user))
+    for user in users]
+
+print() 
+#print("enter the number of user you want to see the efriend tree:")
+#f = int(input())
+
+#print (users[f])   
+
+print( friends_of_friend_ids(users[3])) # Counter({0: 2, 5: 1})
+   
+print()       
+print(data_scientists_who_like("regression"))
+
+print()
+print(most_common_interests_with(users[4]))
+
+print()
+###########################
+#                         #
+# SALARIES AND EXPERIENCE #
+#                         #
+###########################
+salary_by_tenure = defaultdict(list)
+
+for salary, tenure in salaries_and_tenures:
+    salary_by_tenure[tenure].append(salary)
+
+average_salary_by_tenure = {
+    tenure : sum(salaries) / len(salaries)
+    for tenure, salaries in salary_by_tenure.items()
+    }
+
+print()
+print(average_salary_by_tenure)
+
+tenure_bucket_dict  = defaultdict(list)
+
+for salary, tenure in salaries_and_tenures:
+    tenure_bucket_dict[tenure_bucket(tenure)].append(salary)
+
+average_salary_by_bucket = {
+        bucket : sum(salaries) / len(salaries)
+        for bucket, salaries in tenure_bucket_dict.items()
+        }
+
+print()
+print(average_salary_by_bucket)
+
+bucket_list = list(average_salary_by_bucket.items())
+
+bucket_list.sort(key=lambda x : x[1], reverse=True)
+    
+print()
+print(bucket_list)
+
+bucket_max = 0
+bucket_min = 10000000
+
+for i in range(len(bucket_list)-1):
+    
+    if bucket_max < bucket_list[i][1]:
+        bucket_max = bucket_list[i][1]
+        bucket_max_index = i
+        
+    if bucket_min > bucket_list[i][1]:
+        bucket_min = bucket_list[i][1]
+        bucket_min_index = i
+        
+    if bucket_max < bucket_list[i+1][1]:
+        bucket_max = bucket_list[i+1][1]
+        bucket_max_index = i+1
+        
+    if bucket_min > bucket_list[i+1][1]:
+        bucket_min = bucket_list[i+1][1]
+        bucket_min_index = i+1
+        
+        
+    val = (bucket_list[i][1] - bucket_list[i+1][1])/bucket_list[i+1][1]
+    print(r"group ", bucket_list[i][0], " has a ", val*100, r"% differnce from ", \
+          bucket_list[i+1][0])
+
+print(bucket_list[bucket_max_index][0], r"group has:", bucket_list[bucket_max_index][1])
+print(bucket_list[bucket_min_index][0], r"group has:", bucket_list[bucket_min_index][1])
+print(r"Max group is: ", (bucket_list[bucket_max_index][1] - bucket_list[bucket_min_index][1])/bucket_list[bucket_min_index][1], " percent greater")
+
+
+
+
+print()
---
+++ Chapter1Functions.py
@@ -0,0 +1,74 @@
+# -*- coding: utf-8 -*-
+from collections import Counter, defaultdict # not loaded by default
+
+interests = [
+    (0, "Hadoop"), (0, "Big Data"), (0, "HBase"), (0, "Java"),
+    (0, "Spark"), (0, "Storm"), (0, "Cassandra"),
+    (1, "NoSQL"), (1, "MongoDB"), (1, "Cassandra"), (1, "HBase"),
+    (1, "Postgres"), (2, "Python"), (2, "scikit-learn"), (2, "scipy"),
+    (2, "numpy"), (2, "statsmodels"), (2, "pandas"), (3, "R"), (3, "Python"),
+    (3, "statistics"), (3, "regression"), (3, "probability"),
+    (4, "machine learning"), (4, "regression"), (4, "decision trees"),
+    (4, "libsvm"), (5, "Python"), (5, "R"), (5, "Java"), (5, "C++"),
+    (5, "Haskell"), (5, "programming languages"), (6, "statistics"),
+    (6, "probability"), (6, "mathematics"), (6, "theory"),
+    (7, "machine learning"), (7, "scikit-learn"), (7, "Mahout"),
+    (7, "neural networks"), (8, "neural networks"), (8, "deep learning"),
+    (8, "Big Data"), (8, "artificial intelligence"), (9, "Hadoop"),
+    (9, "Java"), (9, "MapReduce"), (9, "Big Data")
+]
+
+user_ids_by_interest = defaultdict(list)
+interests_by_user_id = defaultdict(list)
+
+for user_id, interest in interests:
+    user_ids_by_interest[interest].append(user_id)
+    interests_by_user_id[user_id].append(interest)
+
+def number_of_friends(user):
+    #"""How many friends does _user_ have?"""
+    return len(user["friends"])
+
+def getNumFriends(item):
+    return item[1];
+
+def most_common_interests_with(user):
+    Counter(interested_user_id
+            for interest in interests_by_user_id[user["id"]]
+            for interested_user_id in user_ids_by_interest[interest]
+                if interested_user_id != user["id"])
+            
+################################
+#                              #
+# DATA SCIENTISTS YOU MAY KNOW #
+#                              #
+################################
+def friends_of_friend_ids_bad(user):
+    # "foaf" is short for "friend of a friend"
+    return [foaf["id"]
+            for friend in user["friends"] # for each of user's friends
+            for foaf in friend["friends"]] # get each of _their_ friends
+
+def not_the_same(user, other_user):
+    """two users are not the same if they have different ids"""
+    return user["id"] != other_user["id"]
+
+def not_friends(user, other_user):
+    """other_user is not a friend if he's not in user["friends"];
+    that is, if he's not_the_same as all the people in user["friends"]"""
+    return all(not_the_same(friend, other_user)
+               for friend in user["friends"])
+
+def friends_of_friend_ids(user):
+    return Counter(foaf["id"]
+                   for friend in user["friends"]  # for each of my friends
+                   for foaf in friend["friends"]  # count *their* friends
+                   if not_the_same(user, foaf)    # who aren't me
+                   and not_friends(user, foaf))   # and aren't my friends
+    
+
+def data_scientists_who_like(target_interest):
+    return user_ids_by_interest[target_interest]
+#    return [user_id
+#            for user_id, user_interest in interests
+#                if user_interest == target_interest]
